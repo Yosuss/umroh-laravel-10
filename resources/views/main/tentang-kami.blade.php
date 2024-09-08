acf44a7b4fe7dce1562511b97ca5bc8f65ad5017
Argaaa63 <argaaaa53@gmail.com>
2024-09-08 15:28:22 +0700

    update

--- resources/views/main/tentang-kami.blade.php
+++ resources/views/main/tentang-kami.blade.php
@@ -69,9 +69,12 @@
         <div
             class="flex flex-col-reverse justify-center items-center lg:flex-row gap-2 mt-40 lg:mt-32 m-10 lg:mb-44 lg:m-20">
             <div class="flex justify-center items-center size-full relative">
-                <div class="img bg-gray-200 w-full h-80 lg:h-[40rem] rounded-xl max-lg:mb-20 lg:mx-10">
-                    {{-- <img src="{{ asset('aset/hero.svg') }}" alt="tower" class=" rounded-3xl lg:w-full object-cover"> --}}
-                </div>
+                {{-- <img src="{{ asset('aset/hero.svg') }}" alt="" class=" rounded-3xl lg:w-full object-cover"> --}}
+                <iframe width="850" height="600" class="object-cover"
+                    src="https://www.youtube.com/embed/XJBibv9_FBI?si=pa9E01l5hrh4WVJZ" title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
             <div class="info w-full flex flex-col">
                 <h1 class="lg:text-xl text-medium text-blue-800 w-fit py-2 px-4 rounded-3xl bg-blue-100">Dedikasi Kami,
@@ -113,41 +116,37 @@
         {{-- about-us -end-}}
 
         {{-- visi misi --}}
-        <div
-            class="flex flex-col justify-center items-center lg:flex-row gap-4 my-6 px-4 lg:px-20">
+        <div class="flex flex-col justify-center items-center lg:flex-row gap-4 my-6 px-4 lg:px-20">
             <div class="flex flex-col justify-center size-full relative lg:pl-20">
                 <h1 class="lg:text-xl text-medium text-yellow-800 w-fit py-2 px-4 rounded-3xl bg-yellow-50">Visi Kami
                 </h1>
-                <h1 class="lg:text-3xl text-2xl mt-4 lg:font-semibold lg:w-5/6">“ Menjadi penyedia layanan Umrah dan Haji terkemuka yang dipercaya oleh jamaah, dengan mengutamakan kualitas, kenyamanan, dan keamanan dalam setiap perjalanan spiritual. “</h1>
+                <h1 class="lg:text-3xl text-2xl mt-4 lg:font-semibold lg:w-5/6">“ Menjadi penyedia layanan Umrah dan Haji
+                    terkemuka yang dipercaya oleh jamaah, dengan mengutamakan kualitas, kenyamanan, dan keamanan dalam
+                    setiap perjalanan spiritual. “</h1>
             </div>
             <div class="flex flex-col justify-center size-full relative">
                 <h1 class="lg:text-xl text-medium text-yellow-800 w-fit py-2 px-4 rounded-3xl bg-yellow-50">Misi Kami.
                 </h1>
-                
+
                 <div class="misi">
                     <div class="flex items-center gap-4 mt-6">
-                        <i
-                            class="bi bi-check2 p-1 px-2 rounded-full bg-blue-100 text-blue-800"></i>
+                        <i class="bi bi-check2 p-1 px-2 rounded-full bg-blue-100 text-blue-800"></i>
                         <h1 class="lg:text-2xl text-xl">Menyediakan Layanan Berkualitas</h1>
                     </div>
                     <div class="flex items-center gap-4 mt-4">
-                        <i
-                            class="bi bi-check2 p-1 px-2 rounded-full bg-blue-100 text-blue-800"></i>
+                        <i class="bi bi-check2 p-1 px-2 rounded-full bg-blue-100 text-blue-800"></i>
                         <h1 class="lg:text-2xl text-xl">Menjamin Keamanan dan Kenyamanan</h1>
                     </div>
                     <div class="flex items-center gap-4 mt-4">
-                        <i
-                            class="bi bi-check2 p-1 px-2 rounded-full bg-blue-100 text-blue-800"></i>
+                        <i class="bi bi-check2 p-1 px-2 rounded-full bg-blue-100 text-blue-800"></i>
                         <h1 class="lg:text-2xl text-xl">Mengutamakan Kepuasan Jamaah</h1>
                     </div>
                     <div class="flex items-center gap-4 mt-4">
-                        <i
-                            class="bi bi-check2 p-1 px-2 rounded-full bg-blue-100 text-blue-800"></i>
+                        <i class="bi bi-check2 p-1 px-2 rounded-full bg-blue-100 text-blue-800"></i>
                         <h1 class="lg:text-2xl text-xl">Meningkatkan Mutu Pelayanan</h1>
                     </div>
                     <div class="flex items-center gap-4 mt-4">
-                        <i
-                            class="bi bi-check2 p-1 px-2 rounded-full bg-blue-100 text-blue-800"></i>
+                        <i class="bi bi-check2 p-1 px-2 rounded-full bg-blue-100 text-blue-800"></i>
                         <h1 class="lg:text-2xl text-xl">Meningkatkan Mutu Pelayanan</h1>
                     </div>
                 </div>
@@ -215,7 +214,7 @@
             if (dropdown.classList.contains("hidden")) {
                 document.body.style.overflowY = "";
                 line1.style.transform = 'translateX(0) rotate(0deg)';
-                line2.style.transform = 'translateX(0) rotate(0deg)'; 
+                line2.style.transform = 'translateX(0) rotate(0deg)';
             } else {
                 document.body.style.overflowY = "hidden";
                 line1.style.transform = 'translateX(-50%) rotate(-45deg)';
