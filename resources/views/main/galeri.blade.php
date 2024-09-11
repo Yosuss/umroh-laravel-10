7a28d1b9642671dbdcf469514d3b174133fbf62b
Argaaa63 <argaaaa53@gmail.com>
2024-09-11 12:25:41 +0700

    galeri

--- resources/views/main/galeri.blade.php
+++ resources/views/main/galeri.blade.php
@@ -101,6 +101,23 @@
                 <h1 class="text-2xl lg:text-3xl font-light lg:mt-10">Lorem ipsum dolor sit amet, consectetur adipiscing
                     elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</h1>
             </div>
+            <div class="vid columns-2 lg:columns-3 w-full mt-4 px-4 lg:px-20 gap-2">
+                <video controls class="w-full pb-2 rounded-2xl">
+                    <source src="{{ asset('aset/v1.mp4') }}" type="video/mp4">
+                </video>
+                <video controls class="w-full pb-2 rounded-2xl">
+                    <source src="{{ asset('aset/v2.mp4') }}" type="video/mp4">
+                </video>
+                <video controls class="w-full pb-2 rounded-2xl">
+                    <source src="{{ asset('aset/v3.mp4') }}" type="video/mp4">
+                </video>
+                <video controls class="w-full pb-2 rounded-2xl">
+                    <source src="{{ asset('aset/v4.mp4') }}" type="video/mp4">
+                </video>
+                <video controls class="w-full pb-2 rounded-2xl">
+                    <source src="{{ asset('aset/v5.mp4') }}" type="video/mp4">
+                </video>
+            </div>
         </div>
         {{-- galeri end --}}
 
