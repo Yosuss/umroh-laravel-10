bdb63b04252c821ace8af0774b4c95b2abf9f9e9
Argaaa63 <argaaaa53@gmail.com>
2024-09-12 10:30:56 +0700

    gap wa

--- resources/views/component/whatsapp.blade.php
+++ resources/views/component/whatsapp.blade.php
@@ -1,9 +1,9 @@
         <!-- Icon whatsapp -->
         {{-- <a href="https://api.whatsapp.com/send/?phone=085659670362&text&type=phone_number&app_absent=0"> --}}
             <a href="https://api.whatsapp.com/send/?phone=6281363209449&text&type=phone_number&app_absent=0">
-                <div class="fixed right-10 bottom-10  flex z-20">
+                <div class="fixed right-10 bottom-10  flex z-20 justify-center items-center">
                     <div
-                        class=" bg-white p-2 lg:p-4 rounded-xl w-fit shadow-lg text-xl lg:text-xl flex flex-col justify-center text-center">
+                        class=" bg-white p-4 rounded-xl h-fit shadow-lg text-xl lg:text-xl flex flex-col justify-center text-center">
                         <p class="font-semibold">Cari info umroh?</p>
                         <p class="text-lg font-light lg:mt-2">Hubunngi Sinai aja!</p>
                     </div>
