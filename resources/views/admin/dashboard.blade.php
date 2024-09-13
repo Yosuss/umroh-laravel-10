fc8a8479967d73403ace8ac02e474e9e7e15b812
Argaaa63 <argaaaa53@gmail.com>
2024-09-13 22:29:53 +0700

    @copyright

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,7 +7,7 @@
 <div class="page-content">
     <section class="row">
         <div class="col-12 col-lg-9">
-            <div class="row">
+            {{-- <div class="row">
                 <div class="col-6 col-lg-3 col-md-6">
                     <div class="card">
                         <div class="card-body px-4 py-4-5">
@@ -80,7 +80,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
             <div class="row">
                 <div class="col-12">
                     <div class="card">
--- resources/views/template/layout_admin.blade.php
+++ resources/views/template/layout_admin.blade.php
@@ -733,8 +733,10 @@
                         <p>2023 &copy; Mazer</p>
                     </div>
                     <div class="float-end">
-                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
-                            by <a href="https://saugi.me">Saugi</a></p>
+                        <p>Crafted with <span class="text-danger"></span>
+                            by <a href="">Kompak Team</a></p>
+                        {{-- <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                            by <a href="https://saugi.me">Saugi</a></p> --}}
                     </div>
                 </div>
             </footer>
